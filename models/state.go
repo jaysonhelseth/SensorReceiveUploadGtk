02328d6824b2505b63f0d99e790e4f3d162b31ff
Jayson Helseth <jayson.helseth@gmail.com>
2022-03-07 09:44:29 -0600

    Added code for the rest of the labels in the display.

--- models/state.go
+++ models/state.go
@@ -8,20 +8,33 @@ import (
 )
 
 type State struct {
-	time *gtk.Label
+	time     *gtk.Label
+	pool     *gtk.Label
+	air      *gtk.Label
+	humidity *gtk.Label
+	updated  *gtk.Label
 }
 
 func NewState(gladeBuilder *gtk.Builder) *State {
 	state := &State{}
-	state.time = ui.GetLabel(gladeBuilder, "timeId")
+	state.time = ui.GetLabel(gladeBuilder, "timeValue")
+	state.pool = ui.GetLabel(gladeBuilder, "poolValue")
+	state.air = ui.GetLabel(gladeBuilder, "airValue")
+	state.humidity = ui.GetLabel(gladeBuilder, "humidityValue")
+	state.updated = ui.GetLabel(gladeBuilder, "updatedValue")
 
 	return state
 }
 
 func (s *State) Update() bool {
 	s.time.SetText(fmt.Sprintf("Time: %s",
-		time.Now().Format("2006-01-02 15:04:05")),
-	)
+		time.Now().Format("2006-01-02 15:04:05")))
+
+	s.pool.SetText("00.0")
+	s.air.SetText("00.0")
+	s.humidity.SetText("00.0")
+	s.updated.SetText(fmt.Sprintf("Time: %s",
+		time.Now().Format("2006-01-02 15:04:05")))
 
 	// Return true to keep the timer function happy
 	return true
